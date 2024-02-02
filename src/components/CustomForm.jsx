ba6ce0c48f504b875c5ba2cbb62d7dea775ab370
Jonas Cura <jonasm.cura@gmail.com>
2024-02-02 16:37:21 -0700

    create vite react app and setup

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useState } from 'react'
+import { PlusIcon, TableCellsIcon } from '@heroicons/react/24/solid'
+
+
+
+
+
+export const CustomForm = ( {addTask} ) => {
+  const [task, setTask] = useState("")
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now()
+    })
+    setTask("");
+  }
+
+  return (
+    <form 
+      className='todo' 
+      onSubmit={handleFormSubmit} // controls button
+      >
+      <div className='wrapper'>
+        <input 
+          type='text' 
+          id='task' 
+          className='input'
+          value={task} //
+          onInput={(e) => setTask(e.target.value)} //assures new value gets set real time to input
+          required
+          autoFocus
+          maxLength={60}
+          placeholder='Enter Task'
+          />
+          <label 
+            htmlFor="task"
+            className='label'
+            >Enter Task</label>
+      </div>
+      <button 
+        className='btn'
+        aria-label='Add Task'
+        type='submit'
+      >
+      <PlusIcon />
+
+      </button>
+    </form>
+  )
+}
+
+export default CustomForm
